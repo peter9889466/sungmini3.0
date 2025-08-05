67890895ca37f25acc467d75e796491a0ee671a8
KYJ <nuyoo387@gmail.com>
2025-08-05 09:38:19 +0900

    커밋 테스트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,9 @@ android {
 }
 
 dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
